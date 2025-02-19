84680cf333845cb595b2dff387da6db7cd16aeb7
marrasluca <lmarras10@gmail.com>
2025-02-19 19:31:42 +0100

    add: main

--- src/componenti/Main.jsx
+++ src/componenti/Main.jsx
@@ -1,7 +1,55 @@
+import comics from "../assets/buy-comics-digital-comics.png";
+import merch from "../assets/buy-comics-merchandise.png";
+import shop from "../assets/buy-comics-shop-locator.png";
+import sub from "../assets/buy-comics-subscriptions.png";
+import buy from "../assets/buy-dc-power-visa.svg";
+
+
 function Main() {
     return(
         <main>
-            main
+            <section id="content">
+                <div className="container">
+                    <h2>-- content goes here --</h2>
+                </div>
+            </section>
+
+
+            <section id="information">
+                <div className="container">
+                    <ul>
+                        <li>
+                            <figure>
+                                <img src={comics} alt="" />
+                            </figure>
+                            <span>DIGITAL COMICS</span>
+                        </li>
+                        <li>
+                        <figure>
+                                <img src={merch} alt="" />
+                            </figure>
+                            <span>DC MERCHANDISE</span>
+                            </li>
+                        <li>
+                        <figure>
+                                <img src={sub} alt="" />
+                            </figure>
+                            <span>SUBSCRIPTIONS</span>
+                            </li>
+                        <li>
+                        <figure>
+                                <img src={shop} alt="" />
+                            </figure>
+                            <span>COMIC SHOP LOCATOR</span>
+                            </li>
+                        <li>
+                        <figure>
+                                <img src={buy} alt="" />
+                            </figure>
+                            <span>DC POWER VISA</span></li>
+                    </ul>
+                </div>
+            </section>
         </main>
     )
 }
