535ad9221a49f60f65c646759e7b3d8574ed5376
marrasluca <lmarras10@gmail.com>
2025-02-20 16:28:27 +0100

    add: top footer

--- src/componenti/Footer.jsx
+++ src/componenti/Footer.jsx
@@ -1,7 +1,58 @@
 function Footer() {
     return(
         <footer>
-            Footer
+            
+                <section id="topFooter">
+                <div className="container bg-logo">
+                    <div>
+                    <ul>
+                        <li><a className="title" href="#">DC COMICS</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Tv</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <ul>
+                        <li><a className="title" href="#">SHOP</a></li>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                    </div>
+                    <div>
+                        <ul>
+                        <li><a className="title" href="#">DC</a></li>
+                        <li><a href="#">Terms of use</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Ad</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Talent</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                        <li><a className="title" href="#">SITES</a></li>
+                        <li><a href="#">MAD magazine</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+
+                    
+                    
+                </div>
+                </section>
+                
+                <section id="bottomFooter">
+                <div className="container">
+                bottom footer
+                </div>
+                </section>
+            
         </footer>
     )
 }
--- src/componenti/Header.jsx
+++ src/componenti/Header.jsx
@@ -1,3 +1,16 @@
+// const NavLinks = [
+//     { id: 1, href: '#', text: 'Characters' },
+//     { id: 2, href: '#', text: 'Comics' },
+//     { id: 3, href: '#', text: 'Movies' },
+//     { id: 4, href: '#', text: 'TV' },
+//     { id: 5, href: '#', text: 'Games' },
+//     { id: 6, href: '#', text: 'Collectibles' },
+//     { id: 7, href: '#', text: 'Videos' },
+//     { id: 8, href: '#', text: 'Fans' },
+//     { id: 9, href: '#', text: 'News' },
+//     { id: 10, href: '#', text: 'Shop' },
+//   ]
+
 function Header() {
     return(
         <header>
