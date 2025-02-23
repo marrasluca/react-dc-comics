9bcb60d6d29bd67ec0dc4db8bd480d6f35a757b7
marrasluca <lmarras10@gmail.com>
2025-02-23 16:04:10 +0100

    add: card fumetti

---
+++ src/componenti/ComicCard.jsx
@@ -0,0 +1,13 @@
+const ComicCard  = (props) => {
+    const {comic} = props;
+    const {title, thumb} = comic;
+
+    return (
+        <div className="card">
+            <img src={ thumb } alt={ title }/>
+            <h3>{ title }</h3>
+        </div>
+    );
+}
+
+export default ComicCard;
--- src/componenti/Main.jsx
+++ src/componenti/Main.jsx
@@ -5,12 +5,22 @@ import sub from "../assets/buy-comics-subscriptions.png";
 import buy from "../assets/buy-dc-power-visa.svg";
 
 
+import ComicsList from "./ComicsList"
+
 function Main() {
     return(
         <main>
             <section id="content">
+                <div id="jumbotrone">
+
+
+                </div>
                 <div className="container">
-                    <h2>-- content goes here --</h2>
+
+                    {/* <h2>-- content goes here --</h2> */}
+                    <div>
+                    <ComicsList />
+                    </div>
                 </div>
             </section>
 
